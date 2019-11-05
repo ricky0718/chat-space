9b7000c39786f511441a12116044d585f5371e16
ricky0718 <rikiya0781@gmail.com>
2019-11-05 14:54:20 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,45 +1,27 @@
 $(document).on('turbolinks:load',function(){
   function buildHTML(message){
-    if ( message.image ) {
-    var html = 
-      `<div class="messages-group=${message.id}">
-        <div class="messages-group__detail">
-          <div class="messages-group__detail--name">
-            ${message.user_name}
+      var text=message.content ?  ` ${message.content}` : "";
+      var image=message.image ? `<img src=${message.image} >` : "";
+      var html = 
+        `<div class="messages-group=${message.id}">
+          <div class="messages-group__detail">
+            <div class="messages-group__detail--name">
+              ${message.user_name}
+            </div>
+            <div class="messages-group__detail--day">
+              ${message.date}
+            </div>
           </div>
-          <div class="messages-group__detail--day">
-            ${message.date}
+          <div class="messages-group__comment">
+            <p class="lower-message__content">
+            ${text}
+            </p>
           </div>
-        </div>
-        <div class="messages-group__comment">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
-        </div>
-        <img src=${message.image} >
-      </div>`
-    return html;
-    }
-    else{
-      var html =
-      `<div class="messages-group">
-        <div class="messages-group__detail">
-          <div class="messages-group__detail--name">
-            ${message.user_name}
-          </div>
-          <div class="messages-group__detail--day">
-            ${message.date}
-          </div>
-        </div>
-        <div class="messages-group__comment">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
-        </div>
-      </div>`
+            ${image}
+        </div>`
       return html;
-    };
-  }
+    }
+  
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -58,7 +40,8 @@ $(document).on('turbolinks:load',function(){
       $('.lower-message_content').val('')
       $('.lower-message_image').val('')
       $(".chat-messages").animate({scrollTop:$(".chat-messages")[0].scrollHeight});
-      
+      $('form')[0].reset();
+
     })
     .fail(function(){
     alert("メッセージ送信に失敗しました")
@@ -66,8 +49,6 @@ $(document).on('turbolinks:load',function(){
     })
     .always(function(){
       $('.send').prop("disabled",false);
-      $("#new_message")[0].reset();
-     
     })
   })
 });
